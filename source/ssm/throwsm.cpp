caf090c462f5e8e6acd00511374fe9a33a8809ea
EfeDursun125 <39811406+EfeDursun125@users.noreply.github.com>
2025-03-03 19:00:25 +0000

    Add files via upload

--- source/ssm/throwsm.cpp
+++ source/ssm/throwsm.cpp
@@ -10,7 +10,7 @@ void Bot::ThrowSMUpdate(void)
 	edict_t* ent = nullptr;
 	while (!FNullEnt(ent = FIND_ENTITY_BY_CLASSNAME(ent, "grenade")))
 	{
-		if (ent->v.owner == m_myself && strcmp(STRING(ent->v.model) + 9, "smokegrenade.mdl") == 0)
+		if (ent->v.owner == m_myself && cstrcmp(STRING(ent->v.model) + 9, "smokegrenade.mdl") == 0)
 		{
 			cvar_t* maxVel = g_engfuncs.pfnCVarGetPointer("sv_maxvelocity");
 			if (maxVel)
@@ -22,11 +22,11 @@ void Bot::ThrowSMUpdate(void)
 					fixedVel.x = cclampf(m_throw.x, -fVel, fVel);
 					fixedVel.y = cclampf(m_throw.y, -fVel, fVel);
 					fixedVel.z = cclampf(m_throw.z, -fVel, fVel);
-					if (fixedVel != nullvec)
+					if (!fixedVel.IsNull())
 						ent->v.velocity = fixedVel;
 				}
 			}
-			else if (m_throw != nullvec)
+			else if (!m_throw.IsNull())
 				ent->v.velocity = m_throw;
 
 			FinishCurrentProcess("i have throwed SM grenade");
